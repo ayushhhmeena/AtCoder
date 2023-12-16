0584613353a94d148c0857279252e62ce17e26e1
AYUSH MEENA <87579312+ayushhhmeena@users.noreply.github.com>
2023-12-16 19:26:12 +0530

    Create Three Threes.cpp

---
+++ Three Threes.cpp
@@ -0,0 +1,15 @@
+/*
+Author:- Ayush Meena
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    ll n; cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<n;
+    }
+    return 0;
+}
