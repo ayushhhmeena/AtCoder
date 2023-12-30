245a3736213c283f889277ddd82ec05cc3b9b893
AYUSH MEENA <87579312+ayushhhmeena@users.noreply.github.com>
2023-12-30 14:46:51 +0530

    Create Water Station.cpp

---
+++ Water Station.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+void soln(){
+    ll n; cin>>n;
+    cout<<round(n/5.0)*5;
+}
+int main(){
+    ll t=1; 
+    //cin>>t;
+    while(t--){
+        soln();
+    }
+    return 0;
+}
